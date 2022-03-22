1f53952ae33cf0f357970d90acce1981ebfd849f
BodnarAleksandr <a.bodnar@vipcoingold.com>
2022-03-22 13:03:45 +0200

    first commit

---
+++ cmd/bdjuno/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/forbole/juno/v2/cmd"
+	initcmd "github.com/forbole/juno/v2/cmd/init"
+	parsecmd "github.com/forbole/juno/v2/cmd/parse"
+	"github.com/forbole/juno/v2/modules/messages"
+
+	actionscmd "github.com/forbole/bdjuno/v2/cmd/actions"
+	fixcmd "github.com/forbole/bdjuno/v2/cmd/fix"
+	migratecmd "github.com/forbole/bdjuno/v2/cmd/migrate"
+	parsegenesiscmd "github.com/forbole/bdjuno/v2/cmd/parse-genesis"
+
+	"github.com/forbole/bdjuno/v2/types/config"
+
+	"github.com/forbole/bdjuno/v2/database"
+	"github.com/forbole/bdjuno/v2/modules"
+)
+
+func main() {
+	parseCfg := parsecmd.NewConfig().
+		WithDBBuilder(database.Builder).
+		WithEncodingConfigBuilder(config.MakeEncodingConfig(getBasicManagers())).
+		WithRegistrar(modules.NewRegistrar(getAddressesParser()))
+
+	cfg := cmd.NewConfig("bdjuno").
+		WithParseConfig(parseCfg)
+
+	// Run the command
+	rootCmd := cmd.RootCmd(cfg.GetName())
+
+	rootCmd.AddCommand(
+		cmd.VersionCmd(),
+		initcmd.InitCmd(cfg.GetInitConfig()),
+		parsecmd.ParseCmd(cfg.GetParseConfig()),
+		migratecmd.NewMigrateCmd(),
+		fixcmd.NewFixCmd(cfg.GetParseConfig()),
+		parsegenesiscmd.NewParseGenesisCmd(cfg.GetParseConfig()),
+		actionscmd.NewActionsCmd(cfg.GetParseConfig()),
+	)
+
+	executor := cmd.PrepareRootCmd(cfg.GetName(), rootCmd)
+	err := executor.Execute()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// getBasicManagers returns the various basic managers that are used to register the encoding to
+// support custom messages.
+// This should be edited by custom implementations if needed.
+func getBasicManagers() []module.BasicManager {
+	return []module.BasicManager{
+		simapp.ModuleBasics,
+	}
+}
+
+// getAddressesParser returns the messages parser that should be used to get the users involved in
+// a specific message.
+// This should be edited by custom implementations if needed.
+func getAddressesParser() messages.MessageAddressesParser {
+	return messages.JoinMessageParsers(
+		messages.CosmosMessageAddressesParser,
+	)
+}
---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/forbole/bdjuno/v2
+
+go 1.16
+
+require (
+	github.com/cosmos/cosmos-sdk v0.42.9
+	github.com/forbole/juno/v2 v2.0.0-20220125161451-4f6319e432a5
+	github.com/go-co-op/gocron v1.12.0
+	github.com/gogo/protobuf v1.3.3
+	github.com/jmoiron/sqlx v1.2.1-0.20200324155115-ee514944af4b
+	github.com/lib/pq v1.10.4
+	github.com/pelletier/go-toml v1.9.4
+	github.com/proullon/ramsql v0.0.0-20181213202341-817cee58a244
+	github.com/rs/zerolog v1.26.1
+	github.com/spf13/cobra v1.3.0
+	github.com/stretchr/testify v1.7.0
+	github.com/tendermint/tendermint v0.34.14
+	google.golang.org/grpc v1.42.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
+
+replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
+
+replace google.golang.org/grpc => google.golang.org/grpc v1.33.2
+
+replace github.com/tendermint/tendermint => github.com/forbole/tendermint v0.34.13-0.20210820072129-a2a4af55563d
